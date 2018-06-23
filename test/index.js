ccac083afb2f253df8e7fd2b3a35e4fc3d19b5eb
Pepe Becker <mail@pepebecker.com>
2018-06-23 07:00:00 +0000

    Inncrease test timeout

--- test/index.js
+++ test/index.js
@@ -9,18 +9,18 @@ describe('我 should be the same in both forms', () => {
 	})
 	it('should have the simplified hanzi 我', () => {
 		return mdbg.get('我')
-			.then(data => data.simplified.should.equal('我'))
+		.then(data => data.simplified.should.equal('我'))
 	})
 })
 
 describe('门 should have 2 different forms', () => {
 	it('should have the traditional hanzi 門', () => {
 		return mdbg.get('门')
-			.then(data => data.traditional.should.equal('門'))
+		.then(data => data.traditional.should.equal('門'))
 	})
 	it('should have the simplified hanzi 门', () => {
 		return mdbg.get('門')
-			.then(data => data.simplified.should.equal('门'))
+		.then(data => data.simplified.should.equal('门'))
 	})
 })
 
@@ -35,6 +35,6 @@ describe('喜欢 should have the correct pinyin', () => {
 	})
 	it('欢 in 喜欢 should have the 5th tone', () => {
 		return mdbg.get('喜欢')
-			.then(data => data.definitions['xi3 huan5'].pinyin.should.equal('xǐ huan'))
+		.then(data => data.definitions['xi3 huan5'].pinyin.should.equal('xǐ huan'))
 	})
 })
